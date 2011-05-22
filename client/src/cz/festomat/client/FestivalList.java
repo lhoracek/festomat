3f0b02a52b7cb57ac393a26bd43fa03c2b21c7b0
lhoracek <lhoracek@softeu.com>
2011-05-22 10:37:37 +0200

    init android projektu

---
+++ client/src/cz/festomat/client/FestivalList.java
@@ -0,0 +1,13 @@
+package cz.festomat.client;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FestivalList extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
