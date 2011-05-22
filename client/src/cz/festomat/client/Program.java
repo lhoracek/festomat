378050395ebdd6d9490afd4e6d4574b427093793
Petr Chytil <chytil@gmail.com>
2011-05-22 13:49:18 +0200

    added stupid simple http request for the program data

--- client/src/cz/festomat/client/Program.java
+++ client/src/cz/festomat/client/Program.java
@@ -1,5 +1,14 @@
 package cz.festomat.client;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
@@ -10,38 +19,45 @@ class ProgramFetcher{
 	//program in html:
     private String program;    
     
-    public String getProgram(String url){
-    	return "<html>" +
-    			"<body>" +
-    			"<style type=\"text/css\">"+
-    			"body {color: white; }"  +		
-    			"</style>" +
-    			"<table>" +
-    			"<tr><td><b>Cas</b></td><td><b>Aktivita</b></td></tr>" +
-    			"<tr><td>Rano</td><td>chlast</td></tr>" +
-    			"<tr><td>Vecer</td><td>chlast</td></tr>" + 
-    			"</table>" +
-    			"</body>" +
-    			"</html>";
-    }
-	
+	public String getProgram(String url) {
+		String returnHtml = new String();
+		try {
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpPost httpPost = new HttpPost(url);
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				returnHtml = EntityUtils.toString(httpResponse.getEntity());
+			}
+		} catch (IOException e) {
+			returnHtml = "<html>" +
+					"<body>" +
+					"<style type=\"text/css\">"+
+					"body {color: white; }"  +		
+					"</style>" +
+					"<table>" +
+					"<tr><td><b>Cas</b></td><td><b>Aktivita</b></td></tr>" +
+					"<tr><td>Chyba, nelze nacist data :-/.</td><td>nic</td></tr>" +					 
+					"</table>" +
+					"</body>" +
+					"</html>";
+		}
+		return returnHtml;
+	}
+
 }
 
 public class Program extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //TextView textview = new TextView(this);
-        //textview.setText("This is the Program tab");
         setContentView(R.layout.program);
         
         WebView programWebView = (WebView) findViewById(R.id.program);
         //programWebView.getSettings().setJavaScriptEnabled(true);
         
         programWebView.setBackgroundColor(0);
-        String summary = new ProgramFetcher().getProgram("neconeco");
+        String summary = new ProgramFetcher().getProgram("http://bl00der.kbx.cz/festomat/festomat.html");
         programWebView.loadData(summary, "text/html", "utf-8");
-        
-        //programWebView.loadUrl("http://www.google.com");
+
     }
 }
